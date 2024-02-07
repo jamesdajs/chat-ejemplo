7b91ca35514463177a5dd31f9a6f7dee8a8be21b
daniel jaimes <james.rock.alt@gmail.com>
2024-02-07 11:34:05 -0400

    fix

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
-// server.js
-const app = require('express')();
-const https = require('https');
-//const fs = require('fs');
-//const { Server } = require("socket.io");
+const app = express();
+const server = createServer(app);
 
-/*const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt'),
-}*/
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  //res.sendFile(__dirname + '/index.html');
-  res.send('Hello World!');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-
-
-let server = https.createServer( app)
-/*const io = new Server(server);
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});*/
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('server running at http://localhost:3000');
 });
